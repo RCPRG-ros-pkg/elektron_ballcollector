cdc3b3be15cd130cdd49292240af771e77435825
robot <robot@elektron1.robotyka.ia.pw.edu.pl>
2012-02-29 20:23:36 +0100

    Wstępny dwyetapowy algorytm rozpoznawania piłeczek. Robot podjeżdża do jednej piłeczki i wciąga ją.

--- sensors_processing/src/convert_node.cpp
+++ sensors_processing/src/convert_node.cpp
@@ -13,11 +13,12 @@
 #include <ecl/ipc.hpp>
 
 
-
+#include "../../simple_apps/src/serialswitch.hpp"
 
 #include <dynamic_reconfigure/server.h>
 #include <sensors_processing/TutorialsConfig.h>
 
+#include <std_msgs/Int16.h>
 #include <geometry_msgs/Twist.h>
 #include <std_msgs/Int16MultiArray.h>
 
@@ -45,6 +46,7 @@ std::vector<CIRCLE> circleList;
 double val1 = 0.0;
 double val2 = 0.0;
 int morphIterations = 1;
+int robotRun =1;
 
 
 /*void printGaussianPoiont(Mat &image, int x, int y, int r ){
@@ -70,6 +72,8 @@ using namespace cv;
 namespace enc = sensor_msgs::image_encodings;
 
 
+
+
 bool fristLoop = true;
 Mat firstImage;
 Mat out_image;
@@ -93,8 +97,11 @@ class ImageConverter {
 	image_transport::Publisher image_pub_;
 	image_transport::Subscriber image_depth_sub_;
 	ros::Publisher vel_pub_;
+	ros::Publisher state_pub_;
+
 	int counter;
 
+	int hooverOn;
 public:
 	ImageConverter() :
 			it_(nh_) {
@@ -105,7 +112,12 @@ public:
 
 		vel_pub_ = nh_.advertise<geometry_msgs::Twist> ("cmd_vel", 1);
 
+		state_pub_ = nh_.advertise<std_msgs::Int16> ("state",1);
+
 		counter = 0;
+		hooverOn=0;
+		Semaphore semaphore("test_sem");
+		semaphore.unlock();
 
 #ifdef _ON_PC_
 
@@ -133,7 +145,7 @@ public:
 
 
 	void imageCb(const sensor_msgs::ImageConstPtr& msg) {
-		  ROS_INFO("I heard color image");
+	//	  ROS_INFO("I heard color image");
 
 		cv_bridge::CvImagePtr cv_ptr;
 		try {
@@ -145,21 +157,205 @@ public:
 		}
 
 		Mat gray;
+		int count = 0;
+                double area = 0.0;
+                double area1= 0.0;
+                double x=0.0;
+                double y=0.0;
+                double maxArea=0.0;
 
 		cvtColor(cv_ptr->image, gray, CV_BGR2GRAY);
 
 		Semaphore semaphore("test_sem");
 		semaphore.lock();
-		ROS_INFO("imageCb semaphore in");
+//		ROS_INFO("imageCb semaphore in");
 
-		for(int i=0; i< circleList.size(); ++i ){
+		int circleIndex = -1;
+
+		for(unsigned int j=0; j< circleList.size(); ++j ){
 
 
-			 Point pt1 = Point(circleList[i].x -circleList[i].r, circleList[i].y - circleList[i].r);
-			 Point pt2 = Point(circleList[i].x +circleList[i].r, circleList[i].y + circleList[i].r);
-			 ROS_INFO("draw circle %d (%d,%d) - (%d,%d)",i, pt1.x, pt1.y, pt2.x, pt2.y);
-			 rectangle(gray, pt1, pt2, 255);
+			Point pt1 = Point(circleList[j].x -circleList[j].r, circleList[j].y - circleList[j].r);
+			Point pt2 = Point(circleList[j].x +circleList[j].r, circleList[j].y + circleList[j].r);
+	//		ROS_INFO("draw circle %d (%d,%d) - (%d,%d)",i, pt1.x, pt1.y, pt2.x, pt2.y);
+
+
+			if(pt2.y > 479 || pt2.x > 639){
+				continue;
+			}
+
+			Range rowRange = Range(pt1.y, pt2.y);
+			Range colRange = Range(pt1.x, pt2.x);
+
+	//		ROS_INFO("draw  %d rowRange (%d,%d) - colRange (%d,%d)",i, rowRange.start, rowRange.end, colRange.start, colRange.end);
+	//		ROS_INFO("mat  %d rows %d - cols %d",i, gray.rows, gray.cols);
+
+			Mat currMat = Mat(gray, rowRange, colRange).clone();
+
+			int cols = currMat.cols;
+			int rows = currMat.rows;
+
+
+
+			int backGrnColor =  ( 	(int)currMat.at < uint8_t > (0, 0) +
+									(int)currMat.at < uint8_t > (0, cols-1) +
+									(int)currMat.at < uint8_t > (rows-1, 0) +
+									(int)currMat.at < uint8_t > (rows-1, cols-1))/4;
+
+			int xOffset = pt1.x;
+			int yOffset = pt1.y;
+
+			int ballColor = (int)currMat.at < uint8_t > ((int)rows/2, (int)cols/2);
+
+		/*	if(abs(ballColor - backGrnColor) > 10){
+			//	rectangle(gray, pt1, pt2, 255);
+				continue;
+			}*/
+
+			int trashValue = ( ballColor + backGrnColor )/2;
+
+			threshold(currMat, currMat, trashValue, 255, THRESH_BINARY);
+		//	adaptiveThreshold(currMat, currMat, trashValue, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 5, 2.0);
+
+			std::vector < std::vector<Point> > contours;
+			findContours(currMat, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
+
+		//	ROS_INFO("There are %d contours", contours.size());
+
+			Point2f center(0.f, 0.f);
+			float radius =0.f;
+
+
+
+
+
+			for (int i = 0; i < contours.size(); i++) {
+			//	drawContours(gray, contours, i, 255);
+				 area = contourArea(contours[i]);
+				 //1030
+				if (area < 2030 && area > 100 ) {
+
+					Moments mom = moments(contours[i]);
+
+					double M11 = mom.m11 - (mom.m10 * mom.m01) / mom.m00;
+					double M02 = mom.m02 - (mom.m01 * mom.m01) / mom.m00;
+					double M20 = mom.m20 - (mom.m10 * mom.m10) / mom.m00;
+
+					// for circle it should be ~0.0063
+					double M7 = (M20 * M02 - M11 * M11)
+							/ (mom.m00 * mom.m00 * mom.m00 * mom.m00);
+
+					// circle
+					if (M7 < 0.007 && M7 > 0.0061){
+
+						rectangle(gray, pt1, pt2, 0);
+
+			//			drawContours(cv_ptr->image, contours, i, CV_RGB(255, 0, 255));
+			//			drawContours(gray, contours, i, CV_RGB(255, 0, 255));
+
+						double r = sqrt(area/3.14);
+
+						minEnclosingCircle(contours[i], center, radius);
+						area1 = area;
+
+						//circle(gray, center, r, CV_RGB(255, 0, 255));
+
+						if(area > maxArea){
+							maxArea = area;
+							x = center.x;
+							y = center.y;
+						}
+					//	rectangle(gray, pt1, pt2, 255);
+/*						center.x += xOffset;
+						center.y += yOffset;
+						circle(gray, center , r, 0);*/
+	//					ROS_INFO("%d   circle, M7 = %f , size = %f, odlegosc od kamery = %f, x=%f, y=%f",i,M7, area, 10.5/r, center.x, center.y);
+						++count;
+
+						circleIndex = j;
+
+					//	ballList.push_back(new PosAndId(count, center.x, center.y));
+
+					}
+					// probably circle
+					else if (M7 < 0.0085){
+	//					ROS_INFO("%d    probably circle, M7 = %f, area=%d",i,M7, area);
+
+					}
+					// there is small chance that it's circular, but show them too
+					else if (M7 < 0.011){
+	//					ROS_INFO("%d    no circle, M7 = %f, area=%d",i,M7, area);
+					}
+
+
+				//	ROS_INFO("area0 = %f  area1 = %f", area0, area1);
+				//	ROS_INFO("M7 = %f",M7);
+				}
+			}
+
+/*
+
+			int xOffset = pt1.x;
+			int yOffset = pt1.y;
+			for (unsigned int y = 0; y < rows; ++y) {
+				for (unsigned int x = 0; x < cols; ++x) {
+					gray.at< uint8_t > (yOffset+ y, xOffset+x) = currMat.at < uint8_t > (y, x);
+				}
+			}
+*/
+
+//			ROS_INFO("backGrnColor =   %d ballColor = %d",backGrnColor, ballColor);
+			//threshold(out_image, trash, 75, 255, THRESH_BINARY);
+
+
+
+			 //	treshold wewnatrz kazdego kwadratu
 		}
+
+
+
+		if(count == 1 ){
+					robotRun = 1;
+
+					int circleX = circleList[circleIndex].x;
+					int circleY = circleList[circleIndex].y;//, circleList[j].y
+
+							geometry_msgs::Twist vel;
+							vel.angular.z = (320.0 - (float)circleX)/500.0;
+							vel.linear.x = (0.1 - (abs(320 - circleX) )/3200.0)*(1 - pow(circleY/460.0, 1) );
+
+							vel_pub_.publish(vel);
+							//float linX =
+
+ROS_INFO("circle area1 =%f (x,y)=(%d,%d) vel.linear.x = %f, vel.angular.z = %f", maxArea, circleX, circleY, vel.linear.x, vel.angular.z);
+							if(circleY > 430 && circleX >315 && circleX < 325){
+								ROS_INFO("start hoover");
+								onHoover();
+							//	vel.linear.x = 0;
+								vel.angular.z = 0;
+								vel.linear.x = 0.2;
+								vel_pub_.publish(vel);
+								ros::Duration(1.0).sleep();
+								vel.linear.x = 0;
+								vel_pub_.publish(vel);
+								ros::Duration(1.0).sleep();
+								offHoover();
+								ros::Duration(3.0).sleep();
+								ROS_INFO("stop hoover");
+							}
+							/*
+							else if(maxArea < 800 ){
+							//	offHoover();
+							}
+							*/
+				}
+				else{
+					offHoover();
+					stopRobot();
+				}
+
+
+
 
 
 
@@ -179,13 +375,13 @@ public:
 
 		waitKey(3);
 #endif
-		ROS_INFO("imageCb semaphore out");
+//		ROS_INFO("imageCb semaphore out");
 
 
 	}
 
 	void depthImageCb(const sensor_msgs::ImageConstPtr& msg) {
-	  ROS_INFO("I heard depth image");
+//	  ROS_INFO("I heard depth image");
 
 		cv_bridge::CvImagePtr cv_ptr;
 		try {
@@ -196,15 +392,12 @@ public:
 
 		}
 
-
-
 		Mat gray, trash, orginal;
 
 		cv_ptr->image.convertTo(gray, 	 CV_8UC1, 1./16);
 		cv_ptr->image.convertTo(orginal, CV_16UC1, 1);
 
 		cv_ptr->image.convertTo(gray, 	 CV_8UC1, 1./16);
-
 
 		if(fristLoop){
 			out_image = gray.clone();
@@ -226,19 +419,21 @@ public:
 		}
 
 
+
 		threshold(gray, trash, 190, 255, THRESH_BINARY);
 
 
 		uint16_t pixVal = orginal.at<uint16_t>(200, 320);
 
-		ROS_INFO("(320, 200)=  %d ", (int)pixVal);
+//		ROS_INFO("(320, 200)=  %d ", (int)pixVal);
 
 		//orginal.at<uint16_t>(200, 320)= 60000;
 
 
+
 		pixVal = orginal.at<uint16_t>(400, 320);
 
-		ROS_INFO("(320, 400) =  %d ", (int)pixVal);
+	//	ROS_INFO("(320, 400) =  %d ", (int)pixVal);
 
 
 		std::vector < std::vector<Point> > contours;
@@ -266,7 +461,7 @@ public:
 	//	InputArray src, OutputArray dst, int op, InputArray element, Point anchor=Point(-1,-1), int iterations=1,
 	//	int borderType=BORDER_CONSTANT, const Scalar& borderValue=morphologyDefaultBorderValue()
 
-	//	ROS_INFO("There are %d contours", contours.size());
+//		ROS_INFO("There are %d contours", contours.size());
 
 		Mat empty = gray.clone();
 		threshold(gray, empty, 255, 255, THRESH_BINARY);
@@ -277,7 +472,7 @@ public:
 
 
 
-		 for (int i=0; i<contours.size(); i++) {
+		 for (unsigned int i=0; i<contours.size(); i++) {
 		//	 drawContours( gray, contours[i], color, color, -1, CV_FILLED, 8 );
 			 int area = contourArea(contours[i]);
 
@@ -286,7 +481,7 @@ public:
 			 Point pt1 = Point(639, 479);
 			 Point pt2 = Point(0, 0);
 
-			 for(int j=0; j<contours[i].size(); ++j){
+			 for(unsigned int j=0; j<contours[i].size(); ++j){
 				 pt1.x = min(pt1.x, contours[i][j].x );
 				 pt1.y = min(pt1.y, contours[i][j].y );
 
@@ -335,16 +530,16 @@ public:
 
 		Semaphore semaphore("test_sem");
 		semaphore.lock();
-		ROS_INFO("imageDepthCb semaphore in");
+	//	ROS_INFO("imageDepthCb semaphore in");
 
 		circleList.clear();
 
-		 for (int i=0; i<contours.size(); i++) {
+		 for (unsigned int i=0; i<contours.size(); i++) {
 
 			 Point pt1 = Point(639, 479);
 			 Point pt2 = Point(0, 0);
 
-			 for(int j=0; j<contours[i].size(); ++j){
+			 for(unsigned int j=0; j<contours[i].size(); ++j){
 				 pt1.x = min(pt1.x, contours[i][j].x );
 				 pt1.y = min(pt1.y, contours[i][j].y );
 
@@ -353,7 +548,7 @@ public:
 			 }
 
 			 int a = (pt2.x - pt1.x);
-			 int center_y = (pt1.y + pt2.y)/2;
+//			 int center_y = (pt1.y + pt2.y)/2;
 
 			 CIRCLE circle;
 			 circle.r = a/2;
@@ -365,7 +560,7 @@ public:
 			 circle.x = pt1.x + circle.r;
 			 circle.y = pt1.y + circle.r;
 			 circleList.push_back(circle);
-			 ROS_INFO("push back circle, vector size = %d",circleList.size());
+//			 ROS_INFO("push back circle (%d,%d), r=%d, vector size = %d", circle.x, circle.y, circle.r, (int)circleList.size() );
 
 			 /*
 			 msg = Int16MultiArray()
@@ -373,7 +568,7 @@ public:
 			 msg.data = datadict[signal]*/
 
 		 }
-		ROS_INFO("imageDepthCb semaphore out");
+//		ROS_INFO("imageDepthCb semaphore out");
 		 semaphore.unlock();
 
 
@@ -394,6 +589,43 @@ public:
 
 	//	image_pub_.publish(cv_ptr->toImageMsg());
 	}
+
+        void stopRobot(){
+                if(robotRun == 0)
+                        return;
+                robotRun =0;
+                geometry_msgs::Twist vel;
+                vel.angular.z = 0;
+                vel.linear.x = 0;
+                vel_pub_.publish(vel);
+        }
+
+        void onHoover(){
+                if(hooverOn == 1)
+                        return;
+
+                hooverOn = 1;
+                switchHoover();
+
+        }
+
+        void offHoover(){
+                if(hooverOn == 0)
+                        return;
+
+                hooverOn = 0;
+                //ros::Duration(1.0).sleep();
+                switchHoover();
+
+        }
+
+        void switchHoover(){
+
+                std_msgs::Int16 state;
+                state.data = 1;
+                state_pub_.publish(state);
+
+        }
 };
 
 int main(int argc, char** argv) {
